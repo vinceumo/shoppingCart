e7e5c23e650697f2e9cfa27d34191a5c56002a23
Vincent Humeau <hello@vincent-humeau.com>
2019-06-16 17:36:25 +0100

    Updates

---
+++ precache-manifest.4530640cd094b973698c6dc82b6e77dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shoppingCart/robots.txt"
+  },
+  {
+    "revision": "eb338c165ea3d2d83190",
+    "url": "/shoppingCart/js/chunk-vendors.636e965e.js"
+  },
+  {
+    "revision": "b55be12a8708b97a0dc8",
+    "url": "/shoppingCart/js/app.20a8aabc.js"
+  },
+  {
+    "revision": "ec1f51eb732ce2a009465926fd3a8503",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "b55be12a8708b97a0dc8",
+    "url": "/shoppingCart/css/app.e5e2cfd0.css"
+  },
+  {
+    "revision": "e4e40b0c82d228add33b5bcfe276a859",
+    "url": "/shoppingCart/browserconfig.xml"
+  }
+];
